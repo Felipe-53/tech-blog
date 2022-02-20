95c0bf7ce24c02fb62e0f0b349cfaaecd6119a54
Felipe53 <felipeandresb97@gmail.com>
2022-02-19 22:39:58 -0300

    adding format date use-case

---
+++ use-cases/getFormattedDate.ts
@@ -0,0 +1,23 @@
+import { utcToZonedTime } from 'date-fns-tz';
+
+export function getFormattedDate(date: string | Date) {
+  const months = [
+    'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago',
+    'Set', 'Out', 'Nov', 'Dez'
+  ]
+
+  let dateObj: Date;
+  if (typeof date === 'string') {
+    dateObj = utcToZonedTime(new Date(date), 'America/Recife')
+  }
+  dateObj = utcToZonedTime(date, 'America/Recife')
+
+  let dateNumber = String(dateObj.getDate())
+  if (dateNumber.length === 1) {
+    dateNumber += '0'
+  }
+
+  const monthName = months[dateObj.getMonth()]
+
+  return `${dateNumber} ${monthName}`
+}
