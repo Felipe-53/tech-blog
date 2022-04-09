646aaee1258ce6940b1c43ca9351d4785e55db12
Felipe53 <felipeandresb97@gmail.com>
2022-04-09 00:08:33 -0300

    adds additional fields to TechNote interface

--- types/TechNote.ts
+++ types/TechNote.ts
@@ -2,6 +2,12 @@ interface TechNote {
   id: number
   title: string
   body: string
+  slug: string
+  og_image_url: string
+  author: {
+    name: string
+    href: string
+  }
   created_at: string
   categories: {
     id: number
