2ea2dc8f94d9d8ea2493aa7addd95168c5dbc026
Felipe53 <felipeandresb97@gmail.com>
2022-12-15 08:50:29 -0300

    next codemod

--- components/Introduction/Introduction.tsx
+++ components/Introduction/Introduction.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import img from "../../public/myself.jpg"
-import Image from "next/image"
+import Image from "next/legacy/image";
 import Link from "next/link"
 
 const Introduction: React.FC = () => {
