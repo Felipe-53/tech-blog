203b287abbf6ffd23c599b539da7b238671e0a2b
Felipe53 <felipeandresb97@gmail.com>
2022-12-20 12:23:31 -0300

    changes article presentation hover state

--- components/ArticlePresentation/ArticlePresentation.tsx
+++ components/ArticlePresentation/ArticlePresentation.tsx
@@ -11,18 +11,17 @@ const ArticlePresentation: React.FC<Props> = (props) => {
   const { title, excerpt, created_at, slug } = props
 
   return (
-    (<Link
+    <Link
       href={`/artigos/${slug}`}
       passHref={true}
       onClick={(e) => e.stopPropagation()}
-      className="p-8 hover:border hover:border-primary rounded-md bg-gray-700 hover:cursor-pointer">
-
+      className="p-8 hover:border hover:border-secondary rounded-md bg-gray-700 hover:cursor-pointer"
+    >
       <MediumHeading>{title}</MediumHeading>
       <p className="text-gray-300 text-lg mt-2 mb-4">{created_at}</p>
       <p className="text-darkfont text-lg">{excerpt}</p>
-
-    </Link>)
-  );
+    </Link>
+  )
 }
 
 export default ArticlePresentation
