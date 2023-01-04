b963bcdf82d94dfe48a28b8c1e7b8946109b4b1f
Felipe53 <felipeandresb97@gmail.com>
2023-01-04 12:27:59 -0300

    makes cateogories available to Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,13 @@ import "../styles/globals.css"
 import "highlight.js/styles/rainbow.css"
 import type { AppProps } from "next/app"
 import Layout from "../components/Layout/Layout"
+import { getArticleCategories } from "../utils/categoriesUtils"
+
+const categories = await getArticleCategories()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <Layout categories={categories}>
       <Component {...pageProps} />
     </Layout>
   )
