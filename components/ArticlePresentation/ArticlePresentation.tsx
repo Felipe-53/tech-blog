538e9a2835d7a1f8552a2ef9dd20283dbcb9cd9c
Felipe53 <felipeandresb97@gmail.com>
2023-01-04 16:40:00 -0300

    added category tag to article presentation

--- components/ArticlePresentation/ArticlePresentation.tsx
+++ components/ArticlePresentation/ArticlePresentation.tsx
@@ -2,13 +2,14 @@ import React from "react"
 import Link from "next/link"
 import { Article } from "../../types/Article"
 import MediumHeading from "../typographic/MediumHeading/MediumHeading"
+import CategoryTag from "../CategoryTag/CategoryTag"
 
 interface Props extends Article {
   disableDivider?: boolean
 }
 
 const ArticlePresentation: React.FC<Props> = (props) => {
-  const { title, excerpt, created_at, slug } = props
+  const { title, excerpt, created_at, slug, categories } = props
 
   return (
     <Link
@@ -18,7 +19,14 @@ const ArticlePresentation: React.FC<Props> = (props) => {
       className="p-8 hover:border hover:border-secondary rounded-md bg-gray-700 hover:cursor-pointer"
     >
       <MediumHeading>{title}</MediumHeading>
-      <p className="text-gray-300 text-lg mt-2 mb-4">{created_at}</p>
+
+      <div className="flex text-base h-min gap-4 mt-2">
+        {categories.map((cat) => {
+          return <CategoryTag key={cat.id}>{cat.name}</CategoryTag>
+        })}
+      </div>
+
+      <p className="text-gray-300 text-lg mt-4 mb-4">{created_at}</p>
       <p className="text-darkfont text-lg">{excerpt}</p>
     </Link>
   )
