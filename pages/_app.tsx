4dca5a110eb1f82c33fd10ad31f13e042fbb5898
Felipe53 <felipeandresb97@gmail.com>
2023-01-04 13:25:21 -0300

    category state based on router events

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,27 @@ import Layout from "../components/Layout/Layout"
 import { getArticleCategories } from "../utils/categoriesUtils"
 import { useState } from "react"
 import { Category } from "../types/Category"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
 
 const categories = await getArticleCategories()
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
   const categoryState = useState<Category | null>(null)
+  const [_, set_chosenCategory] = categoryState
+
+  function handleRouteChange() {
+    set_chosenCategory(null)
+  }
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", handleRouteChange)
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  })
 
   pageProps = {
     ...pageProps,
