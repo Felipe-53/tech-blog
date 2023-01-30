7a67a7576c35e57b937993fee854a83de8f158e9
Felipe53 <felipeandresb97@gmail.com>
2023-01-29 23:23:58 -0300

    adds additional env var

--- utils/env.ts
+++ utils/env.ts
@@ -6,6 +6,7 @@ const env = {
   techNoteApiUrl: process.env.TECH_NOTE_API_URL as string,
   techNoteApiToken: process.env.TECH_NOTE_API_TOKEN as string,
   recipientApiUrl: process.env.RECIPIENT_API_URL as string,
+  recipientApiToken: process.env.RECIPIENT_API_TOKEN as string,
 } as const
 
 for (const key of Object.keys(env)) {
