71870cb8c15c69e830384cf7557b36b6f6dc65e1
Felipe53 <felipeandresb97@gmail.com>
2023-01-30 09:24:16 -0300

    original footer

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -2,14 +2,8 @@ import React from "react"
 
 const Footer: React.FC = () => {
   return (
-    <footer className="mt-auto py-6 text-lg text-darkfont bg-primary flex flex-col items-center">
-      <p className="mb-4">
-        Receba atualizações de conteúdo direto na sua caixa de entrada!
-      </p>
-
-      <button className="bg-lime-600 font-bold shadow-2xl py-1 px-4 rounded-md">
-        Inscrever-me
-      </button>
+    <footer className="mt-auto py-2 text-lg text-darkfont bg-primary flex flex-col items-center">
+      <p>Made with ❤️ by Felipe Barbosa</p>
     </footer>
   )
 }
