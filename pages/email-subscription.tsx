5a8e6445e52065ef1866650cb33103a5f4df1c40
Felipe53 <felipeandresb97@gmail.com>
2023-01-29 23:19:40 -0300

    skeleton of email-subcription page

--- pages/email-subscription.tsx
+++ pages/email-subscription.tsx
@@ -1,7 +1,45 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { env } from "../utils/env"
+import { makeFetchJson } from "../utils/fetchJson"
+const fetchJson = makeFetchJson(env.recipientApiUrl, env.recipientApiToken)
 
-const EmailSubscription = () => {
-  return <div>EmailSubscription</div>
+type EmailConfirmedState = "loading" | "success" | "failure"
+
+const EmailSubscription: React.FC = () => {
+  const [emailConfirmed, setEmailConfirmed] =
+    useState<EmailConfirmedState>("loading")
+
+  const params = new URL(document.location.href).searchParams
+  const recipientId = params.get("id")
+
+  useEffect(() => {
+    ;(async () => {
+      if (!recipientId) {
+        setEmailConfirmed("failure")
+        return
+      }
+      try {
+        await fetchJson(`/recipient/${recipientId}/status`, {
+          method: "PATCH",
+        })
+        setEmailConfirmed("success")
+      } catch (err) {
+        console.log(err)
+        setEmailConfirmed("failure")
+      }
+    })()
+  }, [])
+
+  const mapping = {
+    loading: <>Carregando</>,
+    failure: <>Houve um problema, tente novamente</>,
+    success: <>Seu e-mail foi confirmado!</>,
+  }
+  return (
+    <div className="flex items-center justify-center">
+      {mapping[emailConfirmed]}
+    </div>
+  )
 }
 
 export default EmailSubscription
