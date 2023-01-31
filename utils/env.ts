a3d5f26cda9c252008d92f8dd0a4ee9e7f35da39
Felipe53 <felipeandresb97@gmail.com>
2023-01-31 19:51:21 -0300

    remove recipient api url from env because it is public

--- utils/env.ts
+++ utils/env.ts
@@ -5,8 +5,6 @@ const env = {
   articleApiToken: process.env.ARTICLE_API_TOKEN as string,
   techNoteApiUrl: process.env.TECH_NOTE_API_URL as string,
   techNoteApiToken: process.env.TECH_NOTE_API_TOKEN as string,
-  recipientApiUrl: process.env.RECIPIENT_API_URL as string,
-  recipientApiToken: process.env.RECIPIENT_API_TOKEN as string,
 } as const
 
 for (const key of Object.keys(env)) {
