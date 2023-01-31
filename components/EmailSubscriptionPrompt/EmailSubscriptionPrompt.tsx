0710c7d379b9a59d4172cad259ff8af5832c89f4
Felipe53 <felipeandresb97@gmail.com>
2023-01-31 14:35:25 -0300

    design withour material-ui

--- components/EmailSubscriptionPrompt/EmailSubscriptionPrompt.tsx
+++ components/EmailSubscriptionPrompt/EmailSubscriptionPrompt.tsx
@@ -7,19 +7,21 @@ const EmailSubscriptionPrompt: React.FC = () => {
 
   return (
     <div className="text-lg text-darkfont w-full text-center flex flex-col items-center mb-14">
-      <div className="mb-6">
+      <div className="mb-4">
         <p>Gostou do que viu?</p>
         <p>
           Receba atualizações de conteúdo diretamente na sua caixa de entrada!
         </p>
       </div>
 
-      <TextField
-        className="max-w-sm w-full rounded-md"
-        sx={{
-          backgroundColor: colors.gray[700],
-          mb: "1.5rem",
-        }}
+      <input
+        className="max-w-sm w-full rounded-md mb-3
+          bg-gray-700 px-4 py-3
+          hover:outline hover:outline-1  hover:outline-secondary
+          focus:outline focus:outline-1
+          focus:outline-secondary
+
+        "
         color="secondary"
         value={email}
         onChange={(e) => {
@@ -27,9 +29,14 @@ const EmailSubscriptionPrompt: React.FC = () => {
         }}
       />
 
-      <Button variant="contained" size="medium">
-        Enviar
-      </Button>
+      <button
+        className="
+      bg-primary px-8 py-2 rounded-md
+      focus:outline focus:outline-1 focus:outline-secondary
+      "
+      >
+        Inscrever-me
+      </button>
     </div>
   )
 }
